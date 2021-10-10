970cdd7095e194ea5a016e008a999b14add0c1ed
Andrew Gray <37289566+andrewjgray206@users.noreply.github.com>
2021-10-10 12:47:12 +1100

    owner label

--- Pages/Assets/Create.cshtml.cs
+++ Pages/Assets/Create.cshtml.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using InvenManager.Data;
 using InvenManager.wwwroot.Models;
+using InvenManager.Models;
 
 namespace InvenManager.Pages.Assets
 {
